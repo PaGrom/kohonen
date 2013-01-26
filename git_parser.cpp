35e9605fd874eed089da3fcb0907d9faeb039cda
PaGrom <pagrom23@gmail.com>
2013-01-27 03:38:08 +0400

    Add create_commit_file()

--- git_parser.cpp
+++ git_parser.cpp
@@ -4,10 +4,14 @@
 #include <vector>
 #include <sstream>
 
+#define FILE_NAME "cmd_output"
+#define GIT_PATH "~/Dropbox/Work/realclear/linux/"
+#define GIT_LOG "--pretty=tformat:'%cE %H'"
+#define NUM_OF_COMMITS 1000
+
 using namespace std;
 
-vector<string> split_string(string source, char split_char)
-{    
+vector<string> split_string(string source, char split_char) {    
 	vector<string> array;
 
 	istringstream is(source);
@@ -21,12 +25,23 @@ vector<string> split_string(string source, char split_char)
 	return array;
 }
 
-int main(int argc, char const *argv[])
-{
+void create_commit_file() {
+	char command[200];
+	sprintf(command, "cd %s && git log -n %d %s >> %s", GIT_PATH, NUM_OF_COMMITS, GIT_LOG, FILE_NAME);
+	printf("Launch:\n");
+	printf("%s\n", command);
+	system(command);
+}
+
+int main(int argc, char const *argv[]) {
+
 	string source = "A sequence of characters stored consecutively "
 						 "in memory and capable of being processed as a "
 						 "single entity";
 
 	split_string(source,' ');
+
+	create_commit_file();
+
 	return 0;
 }
