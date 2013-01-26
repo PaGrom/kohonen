51e9045c631583e4d4d1fc74477b872884e0c39c
PaGrom <pagrom23@gmail.com>
2013-01-27 02:18:21 +0400

    Start rewrite git parser from Python to C++. Add split_string()

---
+++ git_parser.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> split_string(string source)
+{    
+	std::vector<std::string> array;
+
+	std::istringstream is(source);
+	std::string s;
+	while (std::getline(is, s, ' '))
+		array.push_back(s);
+
+	for (vector<string>::iterator i = array.begin(); i < array.end(); ++i)
+		cout << *i << endl;
+
+	return array;
+}
+
+int main(int argc, char const *argv[])
+{
+	string source = "A sequence of characters stored consecutively "
+						 "in memory and capable of being processed as a "
+						 "single entity";
+
+	split_string(source);
+	return 0;
+}
