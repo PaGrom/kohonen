247aad50ad73d84ed9a73df2a28a3bf51c7b6117
PaGrom <pagrom23@gmail.com>
2013-04-16 09:16:46 +0400

    Added commandline handler

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
 #include "CSOM.hpp"
 
 int main(int argc, char **argv)
 {
 	char usage[200];
-	sprintf(usage, "Usage: %s -p /path/to/git-folder -n number-of-last-commits\n", argv[0]);
-
-	if (argc < 3) { 
-		printf("%s", usage);
-		exit(1); 
-	}
 
 	char *nvalue = NULL;
 	char *pvalue = NULL;
 	int index;
 	int par;
+	int option_index;
 
 	string path_to_git;
 	int num_of_commits;
 	opterr = 0;
 
-	while ((par = getopt (argc, argv, "n:p:")) != -1)
+	const char* short_options = "hn:p:";
+
+	const struct option long_options[] = {
+		{"help",no_argument,NULL,'h'},
+		{"path",optional_argument,NULL,'p'},
+		{NULL,0,NULL,0}
+	};
+
+	while ((par = getopt_long(argc,argv,short_options,
+		long_options,&option_index))!=-1)
 		switch (par) {
+			case 'h':
+				printf("Usage: %s [options] [target] ...\n", argv[0]);
+				printf("Options:\n");
+				printf("   -n,\t\t\tNumber of last commits.\n");
+				printf("   -p,\t--path\t\tPath to git folder\n");
+				printf("   -h,\t--help\t\tPrint this message and exit.\n");
+				break;
 			case 'n':
 				nvalue = optarg;
 				num_of_commits = atoi(nvalue);
@@ -61,7 +75,7 @@ int main(int argc, char **argv)
 				abort();
 		}
 
-	for (index = optind; index < argc; index++)
+	for (index = optind; option_index < argc; option_index++)
 		printf ("Non-option argument %s\n", argv[index]);
 
 	if (!path_to_git.size()) { 
